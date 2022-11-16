9910c438eb429da4fe1b68f74e614935565e8d89
ohoherror <1458251589@qq.com>
2022-11-16 17:59:10 +0800

    lowercase

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package calendar
 import "errors"
 
 type Date struct {
-	Year  int
-	Month int
-	Day   int
+	year  int
+	month int
+	day   int
 }
 
 func (d *Date) SetYear(year int) error {
 	if year < 1 {
 		return errors.New("Invalid year")
 	}
-	d.Year = year
+	d.year = year
 	return nil
 }
 
@@ -20,14 +20,14 @@ func (d *Date) SetMonth(month int) error {
 	if month < 1 || month > 12 {
 		return errors.New("invalid month")
 	}
-	d.Month = month
+	d.month = month
 	return nil
 }
 
-func (d *Date) setDay(day int) error {
+func (d *Date) SetDay(day int) error {
 	if day < 1 || day > 31 {
 		return errors.New("invalid day")
 	}
-	d.Day = day
+	d.day = day
 	return nil
 }
